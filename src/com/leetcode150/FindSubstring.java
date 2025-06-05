6b07d80db240aaad2935f8688a2e46cca4bd77ea
Ashwani <Ashwanigupta2027@gmail.com>
2025-06-05 18:09:39 +0530

    new program

---
+++ src/com/leetcode150/FindSubstring.java
@@ -0,0 +1,26 @@
+package com.leetcode150;
+
+public class FindSubstring {
+
+    public static int findSubstring(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+        int hLen = haystack.length();
+        int nLen = needle.length();
+
+        for (int i = 0; i <= hLen - nLen; i++) {
+            int j = 0;
+            while (j < nLen && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == nLen) return i;
+        }
+        return -1; // not found
+    }
+
+    public static void main(String[] args) {
+        String haystack = "hello world";
+        String needle = "world";
+        System.out.println(findSubstring(haystack, needle));
+
+    }
+}
